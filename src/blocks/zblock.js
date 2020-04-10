bb4024f8b99bfe50db52e2408376475f014e046c
Nic <nicodemus.landaverde98@gmail.com>
2020-04-10 16:00:12 -0400

    Blocks can now spawn from up, down, left, and right

--- src/blocks/zblock.js
+++ src/blocks/zblock.js
@@ -1,28 +1,62 @@
 
 export default class ZBlock {
-	constructor(blockType, gridSize) {
+	constructor(blockType, direction, gridSize) {
 		let z = blockType;
 		
 		this.states = 
 		[ [[z, z, 0],
-		   [0, z, z],  //z[0]
+		   [0, z, z],  //states[0]
 		   [0, 0, 0]],
+		   
 		  [[0, 0, z],
-		   [0, z, z], //z[1]
+		   [0, z, z], //states[1]
 		   [0, z, 0]],
+		   
 		  [[0, 0, 0],
 		   [z, z, 0], //states[2]
 		   [0, z, z]],
+		   
 		  [[0, z, 0],
 		   [z, z, 0], //states[3]
 		   [z, 0, 0]]
 	    ];
 		
-		this.x = Math.trunc(gridSize / 2); //spawnpoint for x
-		this.y = 1;
+		//this.x = Math.trunc(gridSize / 2); //spawnpoint for x
+		this.x = 0;
+		this.y = 0;
 		
-		
+		console.log('d' + direction);
 		this.currentState = this.states[0];
-		this.type = blockType;
+		this.type = blockType; 
+		
+		this.spawnXY(gridSize, direction);
+		
+		
 	}
+	
+	//decide based off direction given, where to spawn the block.
+	spawnXY(gridSize, direction) {
+		let halfway = Math.trunc(gridSize / 2);
+		
+		switch (direction) {
+			case 0: //up
+				this.x = halfway;
+				this.y = 0;
+				break;
+			case 1: //down
+				this.x = halfway;
+				this.y = gridSize - 2; //change later
+				break;
+			case 2: //left
+				this.x = 0;
+				this.y = halfway;
+				break;
+			case 3: //right
+				this.x = gridSize - 3; //this keeps breaking it and i aint sure why.
+				this.y = halfway;
+				break;
+		}
+		
+	}
+	
 }
--- src/logic.js
+++ src/logic.js
@@ -17,24 +17,29 @@ export default class Logic {
         ZBlock      : 7,
         TBlock      : 8
 	  }
+	  
+	  //not sure where direction enum should go.
+	  this.direction = {
+		  Up : 0,
+		  Down: 1,
+		  Left: 2,
+		  Right: 3
+	  }
 	
     this.gridSize = 49;
 	  this.initGrid();
     
     this.elapsedTime = 1;
     this.delay = 1;
-    //we know we need an active block and stuff
-	this.currentBlock = new ZBlock(this.type.ZBlock, this.gridSize); //just a placeholder for now.
-	this.updateCurrentBlock(); //kind of like the main loop, will handle the spawn coordinates and what not.
+	
+    //we know we need an active block and stuff      
+	this.currentBlock = new ZBlock(this.type.ZBlock, this.direction.Up, this.gridSize); //constructor is just a placeholder for now.
+	this.updateCurrentBlock(); 
   }
   
-  //get the state matrix of the current block, then selectively decide
+  //get the state matrix of the current block, then selectively decide which cells need to be filled in.
   updateCurrentBlock() {
-	  let block = this.currentBlock.currentState; //states[]
-	  
-	  //X and Y variables are the offset of the top-left corner of the matrix to the origin (0,0)
-	  let curX = this.currentBlock.x; 
-	  let curY = this.currentBlock.y;
+	  let block = this.currentBlock.currentState; //states[] of the block
 	  
 	  for (let row = 0; row < block.length; row++) {
 		  for (let col = 0; col < block.length; col++) {
@@ -45,11 +50,10 @@ export default class Logic {
 	  }
   }
   
+  //X and Y variables are the offset of the top-left corner of the matrix to the origin (0,0)
   stateMatrixToGrid(row, col) {
 	  let x = this.currentBlock.x;
 	  let y = this.currentBlock.y;
-	  
-	  console.log('x ' + (x + col) + ', y ' + (y + row)); 
 	  this.grid[x + col][y + row] = this.currentBlock.type;
   }
   
